a8b908ce206c2b91aa3869cacc590a9c1c741df2
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-08-21 21:55:53 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -2,17 +2,17 @@
 #include "typewise-alert.h"
 
 TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLow) {
-  TemperatureRange range = {0, 38};  // lower limit 0, upper limit 38
+  TemperatureRange range = {0, 35};  
   EXPECT_EQ(inferBreach(-1, range), TOO_LOW);
 }
 
 TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToHIGH) {
-  TemperatureRange range = {0, 38}; 
+  TemperatureRange range = {0, 35}; 
   EXPECT_EQ(inferBreach(36, range), TOO_HIGH);
 }
 
 TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToNormal) {
-  TemperatureRange range = {0, 38};
+  TemperatureRange range = {0, 35};
   EXPECT_EQ(inferBreach(20, range), NORMAL);
 }
 TEST(TypewiseAlertTest, ClassifyTemperatureBreachPassiveCooling) {
