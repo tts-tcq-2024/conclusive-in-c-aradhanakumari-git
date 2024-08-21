ffe5914d0636524b65ccd677f5fe541188c32c3f
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-08-21 21:26:21 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -4,7 +4,7 @@
 
 /*get temp range based on cooling type*/
 TemperatureRange getTemperatureRange(CoolingType coolingType) {
-  TemperatureRange ranges[] = {
+    TemperatureRange ranges[] = {
     {0, 35}, /*passive cooling*/
     {0, 45}, /*high active cooling*/
     {0, 40}  /*mid active cooling*/
@@ -23,9 +23,7 @@ BreachType inferBreach(double value, TemperatureRange range) {
   return NORMAL;
 }
 
-
-
-/*differentiate temp breach based on cooling type and temp*/
+// differentiate temp breach based on cooling type and temp
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
   TemperatureRange range = getTemperatureRange(coolingType);
   return inferBreach(temperatureInC, range);
@@ -52,7 +50,6 @@ void sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
 }
-
 void sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
   if (breachType == TOO_LOW) {
